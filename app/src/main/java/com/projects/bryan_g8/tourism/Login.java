8da594ebd0ad7bbef4c689edf6e910826637d073
Bryan Gatica <bryangflores@gmail.com>
2016-05-22 16:42:06 -0600

    Sign Up activity created

--- app/src/main/java/com/projects/bryan_g8/tourism/Login.java
+++ app/src/main/java/com/projects/bryan_g8/tourism/Login.java
@@ -22,9 +22,10 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Login extends AppCompatActivity {
+public class
+Login extends AppCompatActivity {
     private TextView txtNick,txtPassword;
-    private Button btnLogin;
+    private Button btnLogin, btnSignUp;
     private Usuario userLogged=null;
 
     @Override
@@ -32,6 +33,7 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         btnLogin=(Button) findViewById(R.id.btnLogin);
+        btnSignUp=(Button) findViewById(R.id.btnSignUp);
         txtNick=(TextView) findViewById(R.id.txtNick);
         txtPassword=(TextView) findViewById(R.id.txtPassword);
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -40,13 +42,14 @@ public class Login extends AppCompatActivity {
                 Map<String,String> params=new HashMap<String, String>();
                 params.put("nick",txtNick.getText().toString());
                 params.put("contraseña",txtPassword.getText().toString());
+
                 JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, WebService.autenticar, new JSONObject(params), new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         try{
                             JSONArray listaUsuarios=response.getJSONArray("user");
                             if(listaUsuarios.length()>0){
-                                Toast.makeText(getApplicationContext(),"Bienvenido",Toast.LENGTH_LONG).show();
+                                //Toast.makeText(getApplicationContext(),"Bienvenido",Toast.LENGTH_LONG).show();
                                 JSONObject user=listaUsuarios.getJSONObject(0);
                                 userLogged=new Usuario(
                                         user.getInt("idUsuario"),
@@ -77,6 +80,11 @@ public class Login extends AppCompatActivity {
                 WebService.getInstance(v.getContext()).addToRequestQueue(request);
             }
         });
-
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,SignUp.class));
+            }
+        });
     }
 }
